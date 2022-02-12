60a1e1647f2baf29e93675966617404f00c52097
Marc Huang <ppman7137@gmail.com>
2022-02-12 16:10:51 +0800

    calculate average review digit length

--- read.py
+++ read.py
@@ -6,14 +6,25 @@ Created on Fri Feb 11 21:05:23 2022
 """
 data = []
 count = 0
+count2 = 0
+length = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
-        data.append(line)
-        count +=1 # count = coiunt + 1
-        if count % 1000 == 0: # count mod 1000 == 0
-           print(len(data))
+        data.append(line) #add string of line to data
+        count +=1 # count = count + 1
+        length = length + len(line) #length of each review of inrequest
+        #length = length + len(data) #length of all data
+        #if count % 1000 == 0: # count mod 1000 == 0
+        if count == 100000:
+          print(line)
+          print(data[count-1])
+          print(len(data))
+print('the average length of each review is', round(length/count,2))
 
-print(len(data))
-print(data[0])
-print('-----------------------------')
-print(data[1])
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print(sum_len)
+           
+#print('all data was read, there are', len(data), 'data')
